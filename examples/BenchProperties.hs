9e93879b3c7e57dc57484fbd39fc578db313a9c6
Luke Bailey <baileylu@tcd.ie>
2026-02-17 19:45:07 +0000

    Remove junk profiling script

--- examples/BenchProperties.hs
+++ examples/BenchProperties.hs
@@ -4,23 +4,29 @@ module Main (main) where
 
 import Data.Time.Clock (diffUTCTime, getCurrentTime)
 import Haskemathesis.Check.Standard (allChecks)
+import Haskemathesis.Config (TestConfig (..), defaultTestConfig)
 import Haskemathesis.Execute.Types (ApiResponse (..))
 import Haskemathesis.OpenApi.Loader (loadOpenApiFile)
 import Haskemathesis.OpenApi.Resolve (resolveOperations)
-import Haskemathesis.Property (propertiesForSpec)
+import Haskemathesis.Property (propertiesForSpecWithConfig)
 import System.IO (hPutStrLn, stderr)
 
 main :: IO ()
-main = do
-    specResult <- loadOpenApiFile "examples/medium-spec.yaml"
-    spec <- case specResult of
-        Left err -> error (show err)
-        Right s -> pure s
-    let cfg = Nothing
-        executor _ = pure (ApiResponse 200 [] "" 0)
-        ops = resolveOperations spec
+  specResult <- loadOpenApiFile "examples/medium-spec.yaml"
+  spec <- case specResult of
+    Left err -> error (show err)
+    Right s -> pure s
+  let cfg = Nothing
+      executor _ = pure (ApiResponse 200 [] "" 0)
+      ops = resolveOperations spec
+  start <- getCurrentTime
+  let props = propertiesForSpec cfg allChecks executor ops
+  end <- getCurrentTime
+  hPutStrLn stderr ("generated properties: " <> show (length props))
+  hPutStrLn stderr ("generation time: " <> show (diffUTCTime end start))
     start <- getCurrentTime
-    let props = propertiesForSpec cfg allChecks executor ops
+    let props = propertiesForSpecWithConfig spec cfg executor ops
     end <- getCurrentTime
-    hPutStrLn stderr ("generated properties: " <> show (length props))
+    let totalProperties = length props * tcPropertyCount cfg
+    hPutStrLn stderr ("generated properties: " <> show totalProperties)
     hPutStrLn stderr ("generation time: " <> show (diffUTCTime end start))
